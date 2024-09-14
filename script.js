f5e70e80e81b26199bcd6f476f009b0009c9d57c
rapo <161580981+rapo84@users.noreply.github.com>
2024-09-14 22:34:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 // Simple JS for potential future interaction or enhancements
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('Sisa\'s Restaurant page loaded successfully!');
+    // Seleccionamos el botón de menú hamburguesa y el menú de navegación
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('nav ul');
+
+    // Añadimos un event listener al botón de menú hamburguesa
+    menuToggle.addEventListener('click', function () {
+        // Alternamos la clase 'active' en el menú para mostrarlo u ocultarlo
+        menu.classList.toggle('active');
+    });
 });
