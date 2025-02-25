797527e3f11733c1db904aa988faef118cf852f3
rapo84 <161580981+rapo84@users.noreply.github.com>
2025-02-25 12:17:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -13,17 +13,26 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuLink = document.querySelector('li > a[href="#"]'); // Enlace "Carta"
     const submenu = document.querySelector('.submenu'); // Submenú que contiene las opciones de carta
 
-    // Añadimos un event listener al enlace de "Carta"
-    menuLink.addEventListener('click', function (event) {
-        event.preventDefault(); // Evitar la recarga de página
-        submenu.classList.toggle('show-submenu'); // Mostrar/ocultar submenú
-    });
+    const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
+    const avisoCookies = document.getElementById('aviso-cookies');
+    const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
 
-    // Opcional: Cerrar el submenú si se hace clic fuera del área del menú
-    document.addEventListener('click', function (event) {
-        if (!menuLink.contains(event.target) && !submenu.contains(event.target)) {
-            submenu.classList.remove('show-submenu'); // Ocultar el submenú si se hace clic fuera
-        }
+    dataLayer = [];
+
+    if (!localStorage.getItem('cookies-aceptadas')) {
+        avisoCookies.classList.add('activo');
+        fondoAvisoCookies.classList.add('activo');
+    } else {
+        dataLayer.push({ 'event': 'cookies-aceptadas' });
+    }
+
+    botonAceptarCookies.addEventListener('click', () => {
+        avisoCookies.classList.remove('activo');
+        fondoAvisoCookies.classList.remove('activo');
+
+        localStorage.setItem('cookies-aceptadas', true);
+
+        dataLayer.push({ 'event': 'cookies-aceptadas' });
     });
 
     // Mensaje de depuración para confirmar que el script se ha cargado correctamente
